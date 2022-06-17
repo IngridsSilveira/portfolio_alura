fc85e4dcb0e17fc9806dc8b8f286eee10f0d8053
Ingrid Souza <92730473+IngridsSilveira@users.noreply.github.com>
2022-06-17 12:03:00 -0300

    adicionei o arquivo js.

---
+++ assets/js/main.js
@@ -0,0 +1,7 @@
+/* BOTÃO PARA ENVIAR MENSAGEM */
+document.getElementsByClassName('btn_contato').addEventListener('click', function(event){
+    event.preventDefault();
+    document.location.href = 'mailto:ingridsouzaok@gmail.com';
+});
+
+/* FORMULÁRIO */
