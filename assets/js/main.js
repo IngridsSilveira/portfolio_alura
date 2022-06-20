d6d59d0a557ccd05e990398554c1a9756be5aa51
Ingrid Souza <92730473+IngridsSilveira@users.noreply.github.com>
2022-06-20 14:44:54 -0300

    adicionei os arquivos de javascript

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,7 +1,54 @@
-/* BOTÃO PARA ENVIAR MENSAGEM */
-document.getElementsByClassName('btn_contato').addEventListener('click', function(event){
+document.getElementById('sendmail').addEventListener('click', function(event){
     event.preventDefault();
-    document.location.href = 'mailto:ingridsouzaok@gmail.com';
-});
+      document.location.href = 'mailto:ingridsouzaok@gmail.com';
+  });
 
-/* FORMULÁRIO */
+
+
+export function valida(input) {
+    const tipoDeInput = input.dataset.tipo
+
+    if(validadores[tipoDeInput]) {
+        validadores[tipoDeInput](input)
+    }
+
+    if(input.validity.valid) {
+        input.parentElement.classList.remove('input-container--invalido')
+        input.parentElement.querySelector('.input-mensagem-erro').innerHTML = ''
+    } else {
+        input.parentElement.classList.add('input-container--invalido')
+        input.parentElement.querySelector('.input-mensagem-erro').innerHTML = mostraMensagemDeErro(tipoDeInput, input)
+    }
+}
+const validadores = {
+}
+const tiposDeErro = [
+    'valueMissing',
+    'typeMismatch',
+    'customError'
+]
+const mensagensDeErro = {
+    nome: {
+        valueMissing: 'O campo de nome não pode estar vazio.'
+    },
+    email: {
+        valueMissing: 'O campo de email não pode estar vazio.',
+        typeMismatch: 'O email digitado não é válido, o padrão de email esperado é (seunome@dominio.com).'
+    },
+    assunto: {
+        valueMissing: 'O campo de assunto não pode estar vazio.',
+    },
+    mensagem: {
+        valueMissing: 'O campo de mensagem não pode estar vazio.',
+    }
+}
+function mostraMensagemDeErro(tipoDeInput, input) {
+    let mensagemErro = ''
+    tiposDeErro.forEach(erro => {
+        if(input.validity[erro]) {
+            mensagemErro = mensagensDeErro[tipoDeInput][erro]
+        }
+    })
+    
+    return mensagemErro
+}
